d7d266cfb8f358a765b38711319b256fd997d794
Arthur Apolinário D'Àvila <arthcc321@gmail.com>
2026-02-05 08:59:46 -0300

    feito o grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { Card } from "./components/Card"
 import { GameHeader } from "./components/GameHeader"
-
+const cardValues =[
+  '🍎', '🍌', "🍇", "🍓", "🍊", "🥝", "🍒", "🍑",
+  "🍎", "🍌", "🍇", "🍓", "🍊", "🥝", "🍒", "🍑"
+]
 function App() {
   return (
-  <div className="app"><GameHeader score= {3} moves={10}/></div>
-  )
+  <div className="app"><GameHeader score= {3} moves={10}/>
+  <div className="cards-grid">
+    {cardValues.map((card)=>(
+      <Card card={card}/>
+    ))}
+  </div>
+  </div>
+  
+)
 }
 
 export default App
